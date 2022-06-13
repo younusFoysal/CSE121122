ce49d3a515de6bfa09f1937c10199ca430eee915
Foysal Ahmed <39677289+younusFoysal@users.noreply.github.com>
2022-06-14 00:03:00 +0600

    Update qus4.c

--- cse122/qus4.c
+++ cse122/qus4.c
@@ -11,51 +11,106 @@ c) List sorted on marks (rank-wise list)
 
 Ans:
 
-
-#include <stdio.h>
-struct student
-{
-char name[50];
-int roll;
-float marks;
-} s[100];
-
+#include<stdio.h>
 int main()
 {
-int i,n;
-struct student s[100];
+    char str[10][10],str2[10];
+    int i,j,n,com;
 
-printf("Enter total of students:\n");
-scanf("%d",&n);
+    printf("Enter a value of n : ");
+    scanf("%d",&n);
 
-for(i=0;i<n;i++)
-{
-printf("\n Enter information of student %d:\n",i+1);
-printf("Enter name: ");
-scanf("%s", s[i].name);
+        printf("Enter strings : \n");
+        for(i=0; i<n; i++)
+        {
+            scanf("%s",str[i]);
+        }
 
-printf("Enter roll number: ");
-scanf("%d", &s[i].roll);
 
-printf("Enter marks: ");
-scanf("%f", &s[i].marks);
-}
 
-printf("Displaying Information:\n");
-for(i=0;i<n;i++)
-{
-printf("\n %d no. student info\n",i+1);
-printf("\tName:%s\n ",s[i].name);
-//or use
-// puts(s[i].name);
+    //sorted name
+        for(j=1;j<n;j++)
+        {
+            for(i=0;i<n-j;i++)
+            {
+                com = strcmp(str[i],str[i+1]);
+                if(com>0)
+                {
+                    strcpy(str2,str[i]);
+                    strcpy(str[i],str[i+1]);
+                    strcpy(str[i+1],str2);
+                }
+            }
+        }
+        /*
+        //print
+        printf("output\n");
+        for(i=0; i<n; i++)
+            printf("%s\n",str[i]);
+    */
 
-printf("\t Roll number: %d\n",s[i].roll);
 
-printf("\t Marks: %.1f\n\n",s[i].marks);
+    //sorted roll
+    int roll[10],min,temp;
 
-}
+    printf("Enter rolls : \n");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&roll[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        min=i;
+        for(j=i+1; j<n; j++)
+        {
+            if(roll[j]<roll[min])
+            {
+                min=j;
+            }
+        }
+        temp = roll[i];
+        roll[i] = roll[min];
+        roll[min] = temp;
+    }
+    /*
+    //print roll
+    printf("output\n");
+    for(i=0; i<n; i++)
+    {
+        printf("%d\n",roll[i]);
+    }
+    */
+    //sorted marks
+    int mark[10],temp1,min1;
+    printf("Enter marks : \n");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&mark[i]);
+    }
 
-return 0;
+    for(i=0; i<n; i++)
+    {
+        min1 = i;
+        for(j=i+1; j<n; j++)
+        {
+            if(mark[j]<mark[min1])
+            {
+                min1=j;
+            }
+        }
+        temp1 = mark[i];
+        mark[i] = mark[min1];
+        mark[min1] = temp1;
+    }
+
+//print result
+printf("Roll\tname\tmark\n");
+    for(i=0; i<n; i++)
+    {
+        printf("%d\t%s\t%d\n",roll[i],str[i],mark[i]);
+    }
+
+    return 0;
 }
 
 
