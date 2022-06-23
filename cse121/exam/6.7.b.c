94f561603288e0482f56c04f2e1543879071c639
Foysal Ahmed <39677289+younusFoysal@users.noreply.github.com>
2022-06-24 01:50:17 +0600

    Added 6.7

---
+++ cse121/exam/6.7.b.c
@@ -0,0 +1,16 @@
+//6.7(b)write programs to print the following outputs using for loops
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+
+    for(i=5; i>=1; i--) {
+        for(k=5; k>i; k--)
+            printf(" ");
+        for(j=1; j<=i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
