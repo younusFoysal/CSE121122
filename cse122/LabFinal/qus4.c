961a6ae17ef1d127978fe2a1c801a8f200805321
Foysal Ahmed <39677289+younusFoysal@users.noreply.github.com>
2022-08-07 04:31:18 +0600

    qus4
    Added

---
+++ cse122/LabFinal/qus4.c
@@ -0,0 +1,37 @@
+/* Write a program that promotes the user for two files, one
+containing a line of text known as source file and other ,an
+empty file known as target file and then copies the contents of
+source file into target file. */
+// Credit goes to Shuvo 
+
+#include<stdio.h>
+
+int main()
+{
+    char id[50],name[50],dep[50];
+
+    FILE *file1,*file2;
+    file1 = fopen("input001.txt","r");
+    file2 = fopen("output001.txt","a");
+
+
+    fscanf(file1,"%s %s %s",id,name,dep);
+
+    printf("%s\t%s\t%s\n",id,name,dep);
+
+    fprintf(file2,"%s\t%s\t%s\n",id,name,dep);
+
+    int i;
+    int ID;
+
+    for(i=1; i<3; i++)
+    {
+        fscanf(file1,"%d %s %s",&ID,name,dep);
+        printf("%d\t%s\t%s\n",ID,name,dep);
+        fprintf(file2,"%d\t%s\t%s\n",ID,name,dep);
+    }
+
+    fclose(file1);
+    fclose(file2);
+}
+
